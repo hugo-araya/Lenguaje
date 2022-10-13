c4acc09a4ed3453fff206d5a2e0c265bb35f5789
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2022-10-13 18:13:12 -0300

    13 oct

---
+++ w_10.py
@@ -0,0 +1,11 @@
+hora = 0
+while hora < 24:
+    minuto = 0
+    while minuto < 60:
+        segundo = 0
+        while segundo < 60:
+            #print(hora,':',minuto,':', segundo)
+            segundo = segundo + 1
+        minuto = minuto + 1
+    hora = hora + 1
+print('Se acabo el dia')
