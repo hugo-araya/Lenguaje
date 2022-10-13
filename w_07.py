59cc87f6ff78d0b51fd991b008e0a5f48503bd11
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2022-10-12 21:38:16 -0300

    12 oct

---
+++ w_07.py
@@ -0,0 +1,6 @@
+numero = int(input('Numero: '))
+i = 1
+while i <= 10:
+    resultado = numero * i
+    print(numero,'*',i,'=',resultado)
+    i = i + 1
---
+++ w_08.py
@@ -0,0 +1,9 @@
+tabla = 1
+while tabla <= 12:
+    i = 1
+    while i <= 10:
+        resultado = tabla * i
+        print(tabla,'*',i,'=',resultado)
+        i = i + 1
+    print()
+    tabla = tabla + 1
