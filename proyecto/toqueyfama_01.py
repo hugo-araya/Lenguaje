c3655d4de8e239063587b86f393c74d39bc6b3e2
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2022-11-23 20:28:32 -0300

    23 nov

--- proyecto/toqueyfama_01.py
+++ proyecto/toqueyfama_01.py
@@ -17,4 +17,33 @@ def genera_objetivo():
     return objetivo
 
 secreto = genera_objetivo()
-print(secreto)
+
+#print(secreto)
+# A continuacion se debe crear una funcion
+# que lea desde el teclado lo ingresado por
+# el jugador. Debe devolver un string
+
+def lee_usuario():
+    combi = input('Combinacion: ')
+    lista = combi.split(' ')
+    combi = "".join(lista)
+    return combi
+
+# Otra version mas artesanal de la lectura
+
+def lee_usuario_1():
+    combi = input('Combinacion: ')
+    salida = ''
+    lista = combi.split(' ') 
+    i = 0
+    while i < len(lista):
+        salida = salida + lista[i]
+        i = i + 1
+    return salida
+
+combinacion = lee_usuario()
+print(combinacion)
+
+combinacion = lee_usuario_1()
+print(combinacion)
+
